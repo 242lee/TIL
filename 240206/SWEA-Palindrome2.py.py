9c4a6168051a12c5215b239d4057159ee5f1f3b2
이소희 <iamurmaster_@naver.com>
2024-02-06 13:56:53 +0900

    실습-회문

--- 240206/SWEA-Palindrome2.py.py
+++ 240206/SWEA-Palindrome2.py.py
@@ -1,17 +1,20 @@
-'''
-N, M = map(int, input().split())
-wordlist = [input() for _ in range(N)]
-#가로
-for i in range(N):
-    for j in range(M):
-        if wordlist[i][j:-1] == wordlist[i][M-j+1:0:-1]:
-            print(wordlist[i])
-'''
-N, M = map(int, input().split())
-wordlist = [input() for _ in range(N)]
-#세로
-for i in range(N):
-    newList = []
-    for j in range(M):
-        for k in range(N):
-            print(newList.append(wordlist[k][j]))
+T = int(input())
+for testcase in range(1, T+1):
+    
+    N, M = map(int, input().split())
+    wordlist = [input() for _ in range(N)]
+    #가로
+    for i in range(N):
+        for j in range(N-M+1):
+            check = wordlist[i][j:j+M]
+            if check == wordlist[::-1]:
+                print(f'{testcase} {check}')
+
+    #세로
+    for j in range(N):
+        for i in range(N-M+1):
+            check = ''
+            for k in range(M):
+                check += wordlist[i+k][j]
+            if check == check[::-1]:
+                print(f'{testcase} {check}')
