20f3ac5bb36233d31b36e39ab3bbbd804bbbcdc1
이소희 <iamurmaster_@naver.com>
2024-02-22 17:20:29 +0900

    240220 update

--- 240220/SWEA-Tree_Practice.py
+++ 240220/SWEA-Tree_Practice.py
@@ -4,7 +4,7 @@ def nodecnt(T):
         nodecnt(leftchild[T])
         nodecnt(rightchild[T])
         cnt += 1
-
+ 
 T = int(input())
 for testcase in range(1, T+1):
     # 노드 1을 루트로 하는 서브트리에 속한 노드 개수 알아내기
@@ -23,12 +23,3 @@ for testcase in range(1, T+1):
             rightchild[p] = c
     nodecnt(N)
     print(f'#{testcase} {cnt}')
-'''
-5 1
-2 1 2 5 1 6 5 3 6 4 
-
-print(parent)       [0, 2, 0, 5, 6, 2, 1]
-print(leftchild)    [0, 6, 1, 0, 0, 3, 4]
-print(rightchild)   [0, 0, 5, 0, 0, 0, 0]
-'''
-    
